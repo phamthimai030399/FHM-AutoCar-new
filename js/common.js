6b886a75458f396e7db4cfa315d609f21d275b41
Nguyễn Thành Chung <chung@swiftpod.com>
2023-07-26 03:00:03 +0700

    no message

--- js/common.js
+++ js/common.js
@@ -1,15 +1,19 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+function startCarousel(selector, numItem = 3) {
+  let items = document.querySelectorAll(selector);
 
-		items.forEach((el) => {
-			const minPerSlide = 3
-			let next = el.nextElementSibling
-			for (var i=1; i<minPerSlide; i++) {
-				if (!next) {
-            // wrap carousel by using first child
-            next = items[0]
-        }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
+  items.forEach((el) => {
+    const minPerSlide = numItem;
+    let next = el.nextElementSibling;
+    for (var i = 1; i < minPerSlide; i++) {
+      if (!next) {
+        // wrap carousel by using first child
+        next = items[0];
+      }
+      let cloneChild = next.cloneNode(true);
+      el.appendChild(cloneChild.children[0]);
+      next = next.nextElementSibling;
     }
-})
+  });
+}
+startCarousel("#bestSellerCarousel.carousel .carousel-item");
+startCarousel("#peopleSayingCarousel.carousel .carousel-item");
